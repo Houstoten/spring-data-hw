5b7b8f557e3179b7df7deaeb197ab00ce975b3f0
Houston <ivan.husarov@gmail.com>
2020-07-10 18:17:28 +0300

    refactor

--- src/main/java/com/bsa/springdata/team/TeamRepository.java
+++ src/main/java/com/bsa/springdata/team/TeamRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface TeamRepository extends JpaRepository<Team, UUID> {
-//    void normalizeName(String name);
-
     int countByTechnologyName(String newTechnology);
 
     Optional<Boolean> findByName(String hipsters_facebook_javaScript);
